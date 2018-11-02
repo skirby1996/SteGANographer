d7065c5e701d8b36f5648bb1527876203fdeabdc
Seamus I Kirby <tug31177@temple.edu>
2018-11-02 13:40:47 -0400

    Implementing production code for Neural Nets (WIP)

--- stegasaurus.py
+++ stegasaurus.py
@@ -77,31 +77,101 @@ def load_model(cfg, model_dir):
         print("\nFinal Results:")
         print("Bob recovered: [%d/%d]" % (bob_passed, num_tests))
 
-def encrypt(cfg, sess, img_path, file_path, out_dir):
+def encrypt(cfg, sess, img_path, msg, out_dir):
     # Create image batch
     with Image.open(img_path) as img:
         width, height = img.size
-        ib = np.zeros(shape=(width * height, cfg.IMG_SIZE, cfg.IMG_SIZE, 3))
-        for w in range(width // cfg.IMG_SIZE):
-            for h in range(height // cfg.IMG_SIZE):
-                img_b = img.crop(box=(w * IMG_SIZE, y * IMG_SIZE, (w + 1) * IMG_SIZE, (h + 1) * IMG_SIZE))
+		x_batches = width // cfg.IMG_SIZE
+		y_batches = height // cfg.IMG_SIZE
+		num_batches = x_batches * y_batches
+        img_batch = np.zeros(shape=(num_batches, cfg.IMG_SIZE, cfg.IMG_SIZE, 3))
+		msg_batch = np.zeros(shape=(num_batches, cfg.IMG_SIZE, cfg.IMG_SIZE, 1))
+        for x in range(x_batches):
+            for y in range(y_batches):
+                img_b = img.crop(box=(x * IMG_SIZE, y * IMG_SIZE, (x + 1) * IMG_SIZE, (y + 1) * IMG_SIZE))
                 data_b = np.asarray(img_b, dtype='float32')
                 data_b /= 255.
-                ib[w * (width // cfg.IMG_SIZE) + h] = data_b
+                img_batch[x * x_batches + y] = data_b
 
     # Create message batch
-
+	num_bytes = (num_batches * IMG_SIZE ** 2) // 8
+	if len(msg) > num_bytes:
+		print("Error: Message exceeds image capacity")
+		return False
+	# Pad msg with spaces
+    msg += (' ' * (num_bytes - len(msg)))
+	for b_ix in range(num_batches):
+		# Get substring
+		msg_b = np.zeros(cfg.IMG_SIZE ** 2)
+		sub_msg = msg[(b_ix * (num_bytes // num_batches)):((b_ix + 1) * (num_bytes // num_batches))]
+		# Write substring bits to msg_batch
+		for c_ix in range(len(msg_b)):
+			c = mb[c_ix]
+            char_val = ord(c)
+            pow = 7
+            while char_val > 0:
+                if char_val >= 2 ** pow:
+                    char_val -= 2 ** pow
+                    msg_b[b_ix][c_ix * 8 + (7 - pow)] = 1.
+                pow -= 1
+				
+		msg_b = (msg_b * 2.) - 1.
+		msg_b = msg_b.reshape((cfg.IMG_SIZE, cfg.IMG_SIZE, 1))
+		msg_batch[b_ix] = msg_b
+				
     # Send batches through Alice
-
+	a_out = sess.run('alice_out:0', feed_dict={
+		'img_in:0': img_batch, 'msg_in:0': msg_batch})
+	
     # Build image from Alice output
-
+	img = Image.new('RGB', (x_batches * cfg.IMG_SIZE, y_batches * cfg.IMG_SIZE))
+	for x in range(x_batches):
+		for y in range(y_batches):
+			b_ix = x * x_batches + y
+			im_b = a_out[b_ix]
+            im_b = (im_b * 255.).astype('uint8')[0]
+            im_b = Image.fromarray(im_b, 'RGB')
+			
+			x_off = x * cfg.IMG_SIZE
+			y_off = y * cfg.IMG_SIZE
+			
+			img.paste(im_bm, (x_off, y_off))
+			
+	new_im.save(img_path[:-4] + "_embedded.bmp"
 
 def decrypt(cfg, sess, img_path, out_dir):
     # Create image batch
-
+    with Image.open(img_path) as img:
+        width, height = img.size
+		x_batches = width // cfg.IMG_SIZE
+		y_batches = height // cfg.IMG_SIZE
+		num_batches = x_batches * y_batches
+        img_batch = np.zeros(shape=(num_batches, cfg.IMG_SIZE, cfg.IMG_SIZE, 3))
+        for x in range(x_batches):
+            for y in range(y_batches):
+                img_b = img.crop(box=(x * IMG_SIZE, y * IMG_SIZE, (x + 1) * IMG_SIZE, (y + 1) * IMG_SIZE))
+                data_b = np.asarray(img_b, dtype='float32')
+                data_b /= 255.
+                img_batch[x * x_batches + y] = data_b
+				
     # Send batches through Bob
-
-    # Build file from Bob output
+	b_out = sess.run('bob_vars_1/bob_eval_out:0',
+        feed_dict={'img_in:0': img_batch})
+	b_out = nn_to_bin(b_out)
+		
+    # Build msg from Bob output
+	bob_out = ""
+	for b_ix in range(num_batches):
+		b = b_out[b_ix]
+		b = b.flatten()
+		for c_ix in range(b.shape[0] // 8):
+            b_char_val = 0
+			for ix in range(8):
+				pow = 7 - ix
+				b_char_val += b_out[8 * c_ix + ix] * 2**pow
+			bob_out += chr(b_char_val)
+		
+	return bob_out
 
 def main():
     cfg = Config()
@@ -137,22 +207,24 @@ def main():
             # Embed using Alice net
             user_in = input("Please enter name of vessel image: ")
             vessel_img_path = os.path.join(in_dir, user_in)
-            user_in = input("Please enter name of target file: ")
-            target_file_path = os.path.join(in_dir, user_in)
-        
-            if os.path.exists(vessel_img_path) and os.path.exists(target_file_path):
-                encrypt(cfg, sess, vessel_img_path, target_file_path, out_dir)
-            else:
-                if not os.path.exists(vessel_img):
-                    print("Error: No image at %s" % vessel_img_path)
-                if not os.path.exists(target_file_path):
-                    print("Error: No file at %s" % target_file_path)
+            target_msg = input("Please enter a message to embed: ")
+			#user_in = input("Please enter string to embed: ")
+            #target_file_path = os.path.join(in_dir, user_in)
+			encrypt(cfg, sess, vessel_img_path, target_msg, out_dir)
+            #if os.path.exists(vessel_img_path) and os.path.exists(target_file_path):
+            #    encrypt(cfg, sess, vessel_img_path, target_file_path, out_dir)
+            #else:
+            #    if not os.path.exists(vessel_img):
+            #        print("Error: No image at %s" % vessel_img_path)
+            #    if not os.path.exists(target_file_path):
+            #        print("Error: No file at %s" % target_file_path)
         else:
             # Extract using Bob net
             user_in = input("Please enter name of embedded image: ")
             embedded_img_path = os.path.join(in_dir, user_in)
             if os.path.exists(embedded_img_path):
-                decrypt(cfg, sess, embedded_img_path, out_dir)
+                recovered = decrypt(cfg, sess, embedded_img_path, out_dir)
+				print("Bob recovered: %s" % recovered)
             else:
                 print("Error: No file at %s" % embedded_img_path)
 
